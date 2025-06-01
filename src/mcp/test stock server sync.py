7f6639c55aec0e5833e7e8ee02dfb27f84854338
Patrick Rotzetter <protzetter@bluewin.ch>
2025-06-01 11:26:38 +0200

    this is the sync version

--- src/mcp/test stock server sync.py
+++ src/mcp/test stock server sync.py
@@ -3,6 +3,7 @@ from mcp.client.sse import sse_client
 from strands import Agent
 from strands.tools.mcp import MCPClient
 from strands.models import BedrockModel
+from strands.models.anthropic import AnthropicModel
 import os, dotenv
 # Add the project root to the path so we can import our modules
 config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../../config/.env')
@@ -11,8 +12,9 @@ dotenv.load_dotenv(config_path)
 region = os.environ.get('BEDROCK_REGION', 'us-east-1')        
 # Get model ID from environment variables or use default
 model= os.environ.get('BEDROCK_MODEL', 'us.amazon.nova-pro-v1:0')
-                      
+ant_model=os.environ.get('ANTHROPIC_MODEL', 'claude-3-7-sonnet-20250219')                      
 print('model:%s',model)
+print('ant_model:%s', ant_model)
 bedrock_model=BedrockModel(
     region_name=region,
     max_tokens=1028,
@@ -21,7 +23,18 @@ bedrock_model=BedrockModel(
         "temperature": 0.7,
     }
 )
-
+key = os.getenv('ANTHROPIC_API_KEY')
+anthropic_model = AnthropicModel(
+    client_args={
+        "api_key": key,
+    },
+    # **model_config
+    max_tokens=1028,
+    model_id=ant_model,
+    params={
+        "temperature": 0.7,
+    }
+)
 # Use both servers together
 def main():
     # Connect to multiple MCP servers
@@ -32,9 +45,9 @@ def main():
             print(t.tool_name)
 
         # Create an agent with all tools
-        agent = Agent(model=bedrock_model, tools=tools)
+        agent = Agent(model=anthropic_model, tools=tools)
         # Run the agent
-        agent('give the trend of the  amazon stock',streaming=False)
+        agent('can you give me a stock market overview ?',streaming=False)
 
 if __name__ == "__main__":
     main()
